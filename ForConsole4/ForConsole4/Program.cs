8f99da9f5339c055ad732f4886ce4b3a84f9ae42
Юрій Поліщук <hdd911@mail.ru>
2016-09-20 22:38:21 +0300

    розвязки лаб 4

---
+++ ForConsole4/ForConsole4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForConsole4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine()),b=int.Parse(Console.ReadLine()),res=0;
+            for (; a <=b; a++)
+                res += a * a;
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ ForConsole5/ForConsole5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForConsole5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()),fact=1,res=0;
+            for (int i = 1; i <= N; i++)
+            {
+                fact *= i;
+                res += fact;
+                
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
